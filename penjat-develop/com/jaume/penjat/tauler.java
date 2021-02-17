34c734d73e3d3d49e56e786f42a37562297ae350
Roberto <rtorres@cifpfbmoll.eu>
2021-02-17 16:55:47 +0100

    3
    3

--- penjat-develop/com/jaume/penjat/tauler.java
+++ penjat-develop/com/jaume/penjat/tauler.java
@@ -1,7 +1,8 @@
 public class tauler{
-    char[] paraulaSecreta;
-    int intents;
-    String[] paraulaEndevinada;
+    private char[] paraulaSecreta;
+    private int intents;
+    private String[] paraulaEndevinada;
+    private int intentsInicials;
 
     public tauler(){
 
@@ -9,17 +10,50 @@ public class tauler{
     public void inicialitzarPartida(String paraula, int num){
         paraulaSecreta = paraula.toCharArray();
         paraulaEndevinada = new String[getParaulaSecreta().length];
+        for (int i = 0;i>paraulaEndevinada.length;i++) {
+            paraulaEndevinada[i] = "_";
+        }
         intents = num;
+        intentsInicials = num;
     }
-
-    public char[] getParaulaSecreta(){
-        return paraulaSecreta;
+    //Getters & Setters
+    public char[] getParaulaSecreta() {
+		return paraulaSecreta;
+	}
+	public void setParaulaSecreta(char[] paraulaSecreta) {
+		this.paraulaSecreta = paraulaSecreta;
+	}
+	public int getIntents() {
+		return intents;
+	}
+	public void setIntents(int intents) {
+		this.intents = intents;
+	}
+	public String[] getParaulaEndevinada() {
+		return paraulaEndevinada;
+	}
+	public void setParaulaEndevinada(String[] paraulaEndevinada) {
+		this.paraulaEndevinada = paraulaEndevinada;
+	}
+	public int getIntentsInicials() {
+		return intentsInicials;
+	}
+	public void setIntentsInicials(int intentsInicials) {
+		this.intentsInicials = intentsInicials;
+	}
+	
+	//Metodos
+	public String imprimir() {
+        String aux = "";
+        for (int i = 0;i<paraulaEndevinada.length;i++) {
+            aux = aux + paraulaEndevinada[i];
+        }
+        return aux;
     }
-    public int getIntents(){
-        return intents;
-    }
+	
+	
+	
     public String verificar(String lletra){
-
         if(lletra.length() != 1){
             return "Lletra incorrecte";
         }
@@ -36,8 +70,5 @@ public class tauler{
             }
             return "";
         }
-    }
-    public String[] getPalabraEndevinada(){
-        return paraulaEndevinada;
     }
 }
